c33b43f3afe2eb165a428a306fa9794a0f7b241a
Madar Areef Hussain Shaik <madshaik@paypal.com>
2018-10-27 17:29:21 +0530

    Updated truffle configuration

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "badge dizzy genius apology unhappy cancel december more decade bomb mango dinosaur";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +9,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/155b4cc430834774937bff90e321a64e")
+      },
+      network_id: 4
+    } 
   }
 };
